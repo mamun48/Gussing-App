3b0831820d7b3c571f24bb839419f02be3e31184
Mamun <mamunmiah193gsc@gmail.com>
2022-08-03 00:24:48 +0600

    Add CountDownTimer

--- app/src/main/java/com/example/android/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/android/guesstheword/MainActivity.kt
@@ -5,9 +5,6 @@ package com.example.android.guesstheword
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-/**
- * Creates an Activity that hosts all of the 
- */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
